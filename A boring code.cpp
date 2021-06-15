5b5964a2bce04b4e3f88da1e16cab63d2f972c41
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-06-15 16:58:00 +0800

    Update A boring code.cpp

--- A boring code.cpp
+++ A boring code.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	printf("Every time God closes a door, he opens a window.\n每次上帝关上一扇门，他就会打开一扇窗。\n");
+	printf("Things have a way of working themselves out.\n船到桥头自然直。\n");
 	printf("\n");
 	system("pause");
 	return 0;
