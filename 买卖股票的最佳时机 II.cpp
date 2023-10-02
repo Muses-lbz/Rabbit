ea53b30f72229366e535de76d693a0032680a847
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2023-10-02 19:49:43 +0800

    Create 买卖股票的最佳时机 II.cpp

---
+++ 买卖股票的最佳时机 II.cpp
@@ -0,0 +1,47 @@
+// 122. 买卖股票的最佳时机 II
+//给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+//在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+//返回 你能获得的 最大 利润 。
+
+//示例 1：
+//输入：prices = [7,1,5,3,6,4]
+//输出：7
+//解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5 - 1 = 4 。
+//     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6 - 3 = 3 。
+//     总利润为 4 + 3 = 7 。
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;        // 初始最大利润为0
+        int minPrice = prices[0]; // 初始最小买入价格为第一天的股价
+
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i]; // 更新最小买入价格
+            }
+            else {
+                int profit = prices[i] - minPrice; // 计算当前卖出价格与最小买入价格的差值
+                maxProfit += profit; // 更新最大利润
+                minPrice = prices[i]; // 更新最小买入价格为当前股价，相当于在同一天买入卖出
+            }
+        }
+
+        return maxProfit;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> prices = { 7, 1, 5, 3, 6, 4 };
+    int maxProfit = solution.maxProfit(prices);
+
+    cout << "最大利润为: " << maxProfit << endl;
+
+    return 0;
+}
